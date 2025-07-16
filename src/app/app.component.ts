4f77188b91854282c98ae33ba06a92c223b45cd2
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2025-07-16 13:35:32 +0530

    Integrate nav-bar component and update theme
    Added pos-nav-bar to the app component and imported NavBarComponent. Updated Angular Material theme to purple-green. Modified nav-bar links and toolbar text for improved navigation and branding. Updated nav-bar component tests to include necessary Angular Material modules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "../app/features/product/product-list/product-list.component";
+// import { ProductListComponent } from "../app/features/product/product-list/product-list.component";
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavBarComponent } from './shared/Components/nav-bar/nav-bar.component'; // Import NavBarComponent
 
 @Component({
   selector: 'pos-root',
-  imports: [ RouterOutlet ],
+  imports: [ RouterOutlet, NavBarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/Components/nav-bar/nav-bar.component.spec.ts
+++ src/app/shared/Components/nav-bar/nav-bar.component.spec.ts
@@ -1,6 +1,11 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavBarComponent } from './nav-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
