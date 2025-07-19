b85fa925ef6218a4fc4030ca52e4e60ac773ba29
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2025-07-19 18:25:50 +0530

    Add product edit component and route
    Introduces ProductEditComponent with form, validation, and service integration for editing products. Updates routing to include the new 'product-edit' path and adds corresponding HTML, SCSS, and unit test files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ProductLineItemListComponent } from './features/product-Line-Item/produ
 import { ProductListComponent } from './features/product/product-list/product-list.component';
 import { ProductCreateComponent } from './features/product/product-create/product-create.component';
 import { ProductDeleteComponent } from './features/product/product-delete/product-delete.component';
+import { ProductEditComponent } from './features/product/product-edit/product-edit.component';
 
 export const routes: Routes = [
     {path: 'product-line-item-list', component: ProductLineItemListComponent },
     {path: 'product-list', component: ProductListComponent },
     {path: 'product-create', component: ProductCreateComponent },
-    {path: 'product-delete', component: ProductDeleteComponent }
+    {path: 'product-delete', component: ProductDeleteComponent },
+    {path: 'product-edit', component: ProductEditComponent },
 ];
---
+++ src/app/features/product/product-edit/product-edit.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductService } from '../../../Core/services/product.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'pos-product-edit',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './product-edit.component.html',
+  styleUrl: './product-edit.component.scss'
+})
+export class ProductEditComponent {
+  productForm: FormGroup;
+  submissionError: boolean = false;
+  message: string = '';
+
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+    this.productForm = this.fb.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      categoryId: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.productForm.valid) {
+      const updatedProduct = this.productForm.value;
+      const productId = updatedProduct.id;
+
+      this.productService.update(productId, updatedProduct).subscribe({
+        next: () => {
+          // Handle successful update
+          this.message = 'Product updated successfully';
+          this.submissionError = false;
+        },
+        error: (error) => {
+          console.error('Error updating product:', error);
+          this.message = 'Error updating product';
+          this.submissionError = true;
+        }
+      });
+    }
+  }
+
+}
+
