d6470c6c178e5eafda53579575896a710744e6f1
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2025-07-19 17:32:30 +0530

    Add product delete component and route
    Introduces ProductDeleteComponent with template, styles, and unit test for deleting products. Updates app routes to include a path for product deletion. The component uses ProductService to handle product deletion by ID and displays success or error messages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductLineItemListComponent } from './features/product-Line-Item/product-Line-Item-List-Create/product-line-item-list/product-line-item-list.component';
 import { ProductListComponent } from './features/product/product-list/product-list.component';
 import { ProductCreateComponent } from './features/product/product-create/product-create.component';
+import { ProductDeleteComponent } from './features/product/product-delete/product-delete.component';
 
 export const routes: Routes = [
     {path: 'product-line-item-list', component: ProductLineItemListComponent },
     {path: 'product-list', component: ProductListComponent },
-    {path: 'product-create', component: ProductCreateComponent }
+    {path: 'product-create', component: ProductCreateComponent },
+    {path: 'product-delete', component: ProductDeleteComponent }
 ];
