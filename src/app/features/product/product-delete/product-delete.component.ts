1f062f9e067d5017e2a26d3e4951002450c88b44
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2025-07-24 05:22:59 +0530

    Add confirmation dialog for product deletion
    Introduced a reusable ConfirmDialogComponent and integrated it into the product delete workflow. Now, users are prompted to confirm before a product is deleted, improving safety and user experience.

--- src/app/features/product/product-delete/product-delete.component.ts
+++ src/app/features/product/product-delete/product-delete.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ProductService } from '../../../Core/services/product.service';
 import { CommonModule } from '@angular/common';
 import { CardWrapperComponent } from "../../../shared/Components/card-wrapper/card-wrapper.component"; 
+import { ConfirmDialogComponent } from '../../../shared/dialogs/confirm-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'pos-product-delete',
@@ -12,10 +15,22 @@ import { CardWrapperComponent } from "../../../shared/Components/card-wrapper/ca
 export class ProductDeleteComponent {
   message: string = '';
   isError: boolean = false;
+  productId: string = '';
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private dialog: MatDialog) { }
+  openConfirmDialog(productId : string){
+    const dialogRef = this.dialog.open(ConfirmDialogComponent,{
+      data : {message : 'Are you sure you want to delete this Product ?', title : 'Delete Product', confirmButtonText : 'Delete', cancelButtonText : 'Cancel'}
+    });
+    dialogRef.afterClosed().subscribe(result =>{
+      if (result){
+        this.deleteProduct(productId)
+      }
+    })
+  }
+
   // Method to delete a product by its ID
-    deleteProduct(productId: string) {
+    deleteProduct(productId : string) {
       this.productService.delete(productId).subscribe({
         next: () => {
           this.message = 'Product deleted successfully';
---
+++ src/app/shared/dialogs/confirm-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'pos-confirm-dialog',
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {title?: string, message: string, confirmButtonText?: string, cancelButtonText?: string}
+  ) { }
+  onClose(result: boolean): void {
+    this.dialogRef.close(result);
+  }
+}
