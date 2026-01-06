13265955d7defa75aaf1a5afb5db37c1d4176417
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2026-01-06 14:20:42 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7202/api'
+  apiUrl: 'https://thisara-pos-api.onrender.com/api'
 };
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
