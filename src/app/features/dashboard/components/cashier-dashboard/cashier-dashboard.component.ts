f81c9e2cdc582b5179b29d2caa4728fecb43c362
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2026-01-27 11:57:54 +0530

    Created the directory structure for the role based dashboard feature

---
+++ src/app/features/dashboard/components/cashier-dashboard/cashier-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pos-cashier-dashboard',
+  imports: [],
+  templateUrl: './cashier-dashboard.component.html',
+  styleUrl: './cashier-dashboard.component.scss'
+})
+export class CashierDashboardComponent {
+
+}
---
+++ src/app/features/dashboard/components/stock-clerk-dashboard/stock-clerk-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pos-stock-clerk-dashboard',
+  imports: [],
+  templateUrl: './stock-clerk-dashboard.component.html',
+  styleUrl: './stock-clerk-dashboard.component.scss'
+})
+export class StockClerkDashboardComponent {
+
+}
