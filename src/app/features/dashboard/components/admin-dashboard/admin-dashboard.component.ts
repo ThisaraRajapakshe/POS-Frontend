a477237527025b7651a46c1488b49e59f25cf294
Thisara Rajapakshe <thisararajapakshe2020@gmail.com>
2026-02-06 05:53:18 +0530

    Created Demo Admin Dashboard
    Created a demo admin dashboard with sample data.

--- src/app/features/dashboard/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/features/dashboard/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+interface AdminStats {
+  totalRevenue: number;
+  ordersCount: number;
+  lowStockItems: number;
+  activeStaff: number;
+}
 @Component({
   selector: 'pos-admin-dashboard',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrl: './admin-dashboard.component.scss',
 })
-export class AdminDashboardComponent {
-
+export class AdminDashboardComponent implements OnInit {
+  // Signal to hold dashboard state.
+  // In a real app, this would be populated by a Service call.
+  stats = signal<AdminStats>({
+    totalRevenue: 0,
+    ordersCount: 0,
+    lowStockItems: 0,
+    activeStaff: 0,
+  });
+  date! : Date;
+  constructor() {
+    setInterval(() => {
+      this.date = new Date()
+    }, 1000);
+  }
+  ngOnInit() {
+    // SIMULATION: Fetching data from an API
+    // Replace this with: this.dashboardService.getStats().subscribe(...)
+    setTimeout(() => {
+      this.stats.set({
+        totalRevenue: 125430,
+        ordersCount: 45,
+        lowStockItems: 3,
+        activeStaff: 5,
+      });
+    }, 500);
+  }
 }
