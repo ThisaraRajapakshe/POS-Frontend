10d6a5707d0a876d2052f1e3891d9957e28ebda2
Thisara Rajapakshe <thisararajapakshe2020@gmail.com>
2026-02-23 16:19:42 +0530

    added service for user registration

---
+++ src/app/auth/register/services/user-register.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core';
+import { addStaffRequestDto } from '../register.models';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegisterService {
+  private readonly apiUrl = `${environment.apiUrl}/Auth/register`;
+  private http = inject(HttpClient);
+  constructor() { }
+
+  registerUser(userData: addStaffRequestDto) {
+    return this.http.post(this.apiUrl, userData);
+    
+  }
+}
