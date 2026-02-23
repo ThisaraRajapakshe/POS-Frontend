0b6e794d60799707f58e46c3a854645ea4fec620
Thisara Rajapakshe <thisararajapakshe2020@gmail.com>
2026-02-23 06:51:36 +0530

    created the file structure and nessacery files

---
+++ src/app/features/staff/components/staff-form/staff-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pos-staff-form',
+  imports: [],
+  templateUrl: './staff-form.component.html',
+  styleUrl: './staff-form.component.scss'
+})
+export class StaffFormComponent {
+
+}
